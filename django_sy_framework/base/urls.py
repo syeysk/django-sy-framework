eaf29c5bde7d7585bb5d27d9de533be287073ef0
shyzik93 <shyzik93@mail.ru>
2023-08-06 01:30:17 +0300

    add ProfileView

--- django_sy_framework/base/urls.py
+++ django_sy_framework/base/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from .views import (
     IntroView,
     MicroservicesListView,
+    ProfileView,
 )
 
 
 urlpatterns = [
     path('', IntroView.as_view(), name='index'),
     path('all-microservices/', MicroservicesListView.as_view(), name='microservices'),
+    path('profile/', ProfileView.as_view(), name='custom_profile'),
 ]
--- django_sy_framework/base/views.py
+++ django_sy_framework/base/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .serializers import ProfileViewSerializer
 
 
 class IntroView(APIView):
@@ -10,3 +15,30 @@ class IntroView(APIView):
 class MicroservicesListView(APIView):
     def get(self, request):
         return render(request, 'base/list_of_microservices.html')
+
+
+class ProfileView(LoginRequiredMixin, APIView):
+    def get(self, request):
+        context = {}
+        return render(request, 'base/profile.html', context)
+
+    def post(self, request):
+        serializer = ProfileViewSerializer(data=request.POST)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        user = request.user
+        update_fields = []
+        if user.first_name != data['first_name']:
+            user.first_name = data['first_name']
+            update_fields.append('first_name')
+
+        if user.last_name != data['last_name']:
+            user.last_name = data['last_name']
+            update_fields.append('last_name')
+
+        if update_fields:
+            user.save()
+
+        result_data = {'success': True, 'updated': update_fields}
+        return Response(status=status.HTTP_200_OK, data=result_data)
