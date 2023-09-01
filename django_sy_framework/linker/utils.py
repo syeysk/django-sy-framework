e6533c6becda3ee3fad301de1f3b15ec4ad1bcd9
shyzik93 <shyzik93@mail.ru>
2023-09-02 02:19:56 +0300

    add `url_new`, `link_instance_from_request` for microservices

---
+++ django_sy_framework/linker/utils.py
@@ -0,0 +1,15 @@
+from django_sy_framework.linker.models import Linker
+
+
+def link_instances(link_to: str, link_to_id: int, instances: list['django.db.models.Model']):
+    linkers = (Linker(link_to=link_to, link_to_id=link_to_id, content_object=instance) for instance in instances)
+    Linker.objects.bulk_create(linkers, batch_size=100)
+
+
+def link_instance_from_request(instance, request):
+    link_to_parametr = request.GET.get('link_to')
+    if link_to_parametr:
+        link_to_parts = link_to_parametr.split('-')
+        if len(link_to_parts) == 2:
+            link_to, link_to_id = link_to_parts
+            link_instances(link_to, int(link_to_id), [instance])
--- django_sy_framework/linker/views.py
+++ django_sy_framework/linker/views.py
@@ -7,10 +7,11 @@ from rest_framework import status
 
 from django_sy_framework.linker.models import Linker
 from django_sy_framework.linker.serializers import LinkerGetViewSerializer, LinkerPutViewSerializer
+from django_sy_framework.linker.utils import link_instances
 
 
 class LinkerView(APIView):
-    def post(self, request, link_to: str, link_to_id: str):
+    def post(self, request, link_to: str, link_to_id: int):
         serializer = LinkerGetViewSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
@@ -37,27 +38,26 @@ class LinkerView(APIView):
             }
             linked_objects.append(linked_object)
 
+        linked_model = getattr(Linker, data['object']).rel.related_model
         response_data = {
             'objects': linked_objects,
             'page': data['p'],
             'num_pages': paginator.num_pages,
+            'url_new': linked_model().url_new,
         }
         return Response(status=status.HTTP_200_OK, data=response_data)
 
-    def put(self, request, link_to: str, link_to_id: str):
+    def put(self, request, link_to: str, link_to_id: int):
         serializer = LinkerPutViewSerializer(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
-
-        linkers = []
+        querysets = []
         for linker in serializer.validated_data:
             linked_model = getattr(Linker, linker['object']).rel.related_model
-            linked_objects = linked_model.objects.filter(pk__in=linker['object_ids']).only('pk')
-            for linked_object in linked_objects:
-                linkers.append(
-                    Linker(link_to=link_to, link_to_id=link_to_id, content_object=linked_object),
-                )
+            querysets.append(linked_model.objects.filter(pk__in=linker['object_ids']).only('pk'))
 
-        Linker.objects.bulk_create(linkers, batch_size=100)
+        if querysets:
+            link_instances(link_to, link_to_id, chain(*querysets))
+
         return Response(status=status.HTTP_202_ACCEPTED)
 
     def delete(self, request, link_to: str, link_to_id: str):
