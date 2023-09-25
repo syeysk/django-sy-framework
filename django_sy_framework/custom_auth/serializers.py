b227d5a653cb4392a6ca4d19f80c91bfb272a7d9
shyzik93 <shyzik93@mail.ru>
2023-09-26 01:11:11 +0300

    improve checking password

--- django_sy_framework/custom_auth/serializers.py
+++ django_sy_framework/custom_auth/serializers.py
@@ -1,11 +1,30 @@
+from re import match
 from rest_framework import serializers
 
 
 class RegistrationSerializer(serializers.Serializer):
-    password1 = serializers.CharField(min_length=1, max_length=50)
-    password2 = serializers.CharField(min_length=1, max_length=50)
+    PASSWORD_VALIDATION_ERROR = (
+        'Пароль должен содержать следующие группы символов: цифры, заглавные и строчные буквы и спецсимволы _/-'
+    )
+
+    def validate_password1(self, value):
+        for template in ('[0-9]', '[a-z]', '[A-Z]', '[_-]'):
+            if not match(template, value):
+                raise serializers.ValidationError(self.PASSWORD_VALIDATION_ERROR)
+
+        return value
+
+    def validate_data(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError('Пароли должны совпадать')
+
+        return data
+
+
+    password1 = serializers.CharField(min_length=10, max_length=50)
+    password2 = serializers.CharField(min_length=10, max_length=50)
     username = serializers.SlugField(min_length=1, max_length=50)
-    email = serializers.EmailField(min_length=1, max_length=50)
+    email = serializers.EmailField(min_length=3, max_length=50)
 
 
 class AddTokenSerializer(serializers.Serializer):
