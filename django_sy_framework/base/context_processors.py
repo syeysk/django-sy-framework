833d12b3863e66cc051ef0bebfc7c1f914fbb55a
shyzik93 <shyzik93@mail.ru>
2023-10-12 21:37:30 +0300

    add context preprocessor

---
+++ django_sy_framework/base/context_processors.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+
+def settings_variables(request):
+    return {
+        'metric_system_code': getattr(settings, 'METRIC_SYSTEM_CODE', ''),
+        'debug': settings.DEBUG,
+    }
