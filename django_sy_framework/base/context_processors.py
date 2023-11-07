431866637066ad19246cc87e361af4455d58e8c6
shyzik93 <shyzik93@mail.ru>
2023-11-07 04:28:34 +0300

    delete microservice menu, redesign

--- django_sy_framework/base/context_processors.py
+++ django_sy_framework/base/context_processors.py
@@ -2,8 +2,13 @@ from django.conf import settings
 
 
 def settings_variables(request):
+    show_metric = True
+    if request.user.is_authenticated:
+        show_metric = request.user.username not in getattr(settings, 'HIDE_METRIC_FOR', [])
+
+    metric_system_code = getattr(settings, 'METRIC_SYSTEM_CODE', '') if show_metric else ''
     return {
-        'metric_system_code': getattr(settings, 'METRIC_SYSTEM_CODE', ''),
+        'metric_system_code': metric_system_code,
         'debug': settings.DEBUG,
         'site_url': settings.SITE_URL,
     }
